9403985b0fc0ee86f5c6b98da253c7deca5827b5
Saksham Shukla <sakshamshukla2005@gmail.com>
2024-07-17 23:22:06 +0530

    update

---
+++ Memory management/malloc.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int *ptra;
+    ptra = (int*)malloc(5*sizeof(int));
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter the value at element number %d: \n",i);
+        scanf("%d", &ptra[i]);
+    }
+    
+    for (int i = 0; i < 7; i++)
+    {
+        printf("The value at element number %d is: %d\n", i, ptra[i]);
+    }
+
+    free(ptra);
+    
+    return 0;
+        
+}
